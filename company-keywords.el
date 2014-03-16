e2451d3e6c5fe33977429e663fadb2eef7b35837
Dmitry Gutov <dgutov@yandex.ru>
2014-03-16 05:20:37 +0200

    company-keywords-alist: add missing question mark

--- company-keywords.el
+++ company-keywords.el
@@ -203,7 +203,7 @@
      "except" "exec" "finally" "for" "from" "global" "if" "import" "in" "is"
      "lambda" "not" "or" "pass" "print" "raise" "return" "try" "while" "yield")
     (ruby-mode
-     "BEGIN" "END" "alias" "and"  "begin" "break" "case" "class" "def" "defined"
+     "BEGIN" "END" "alias" "and"  "begin" "break" "case" "class" "def" "defined?"
      "do" "else" "elsif"  "end" "ensure" "false" "for" "if" "in" "module"
      "next" "nil" "not" "or" "redo" "rescue" "retry" "return" "self" "super"
      "then" "true" "undef" "unless" "until" "when" "while" "yield")
