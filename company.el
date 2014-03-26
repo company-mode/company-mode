company-mode/company-mode

commit 10069f3738be871d2d54e5b4dc10d4f69a7295a1

Regression fix.
- Properly fix `company-preview-frontend', amend last two commits.

- When tooltip spans text with multi-line `display' property and
  e-o-b, insufficient newlines may be padded.

- Calling `recenter' with tooltip open may cause tooltip update to
  fail.

* company.el:

(company-pseudo-tooltip-show): Use overlay to disable `display'
property for region from (point) to (point-max) prior to padding
new lines, then adjust overlay to proper size. Do not maintain
`company-row', as certain commands (e.g. `recenter') may change
screen row number without triggering tooltip redraw. This
information can be inferred from the first element of
`company-line-overlays'.

(company-pseudo-tooltip-edit): Remove reference to `company-row',
use `overlay-start' and `vertical-motion' to setup call to
`company--create-line-overlay'. `(vertical-motion 0)' is safe here
because we are inside the outer overlay which disabled the
`display' property of the region.

(company-preview-show-at-point): Check for `(eobp)' and handle
accordingly.

* company-tests.el(company-preview-show-with-annotations): Update
  to reflect change.

company.el
@@ -2094,37 +2094,35 @@ current window."
            (height (length lines))
            (adjcol (company--adjust-column column width))
            (inhibit-point-motion-hooks t)
+           (beg (point))
            (firstrow (if above (- row height) row))
-           (beg (progn
-                  (move-to-window-line firstrow)
-                  (point)))
-           (lastrow (progn
-                      (move-to-window-line (+ firstrow height))
-                      (company--row)))
-           (pad (- (+ firstrow height) lastrow))
            (modified (buffer-modified-p))
-           (padstart (point))
-           (padend (if (> pad 0)
-                       (let ((buffer-undo-list t)
-                             (inhibit-modification-hooks t))
-                         (goto-char (point-max))
-                         (insert (make-string pad ?\n))
-                         (setq company--point-max (point-max)))
-                     padstart))
-           (tick (buffer-chars-modified-tick))
-           (ovl (progn (overlay-recenter padend) (make-overlay beg padend)))
-           (line-overlays
-            (progn
-              (overlay-put ovl 'priority 99)
-              (overlay-put ovl 'display t)
-              (move-to-window-line firstrow)
-              (mapcar
-               (apply-partially 'company--create-line-overlay adjcol)
-               lines))))
+           (ovl (make-overlay beg (point-max)))
+           lastrow pad padstart tick line-overlays)
+
+      (overlay-put ovl 'priority 99)
+      (overlay-put ovl 'display t)
+      (move-to-window-line (+ firstrow height))
+      (setq lastrow (company--row)
+            pad (- (+ firstrow height) lastrow)
+            padstart (point))
+      (if (> pad 0)
+          (let ((buffer-undo-list t)
+                (inhibit-modification-hooks t))
+            (goto-char (point-max))
+            (insert (make-string pad ?\n))
+            (setq company--point-max (point-max)))
+        (move-overlay ovl beg padstart))
+      (setq tick (buffer-chars-modified-tick))
+      (overlay-recenter (overlay-end ovl))
+      (move-to-window-line firstrow)
+      (setq line-overlays
+            (mapcar
+             (apply-partially 'company--create-line-overlay adjcol)
+             lines))
 
       (overlay-put ovl 'company-width width)
       (overlay-put ovl 'company-height (if above (- height) height))
-      (overlay-put ovl 'company-row firstrow)
       (overlay-put ovl 'company-column column)
       (overlay-put ovl 'company-tick tick)
       (overlay-put ovl 'company-modified modified)
@@ -2141,7 +2139,6 @@ current window."
 
 (defun company-pseudo-tooltip-edit (selection)
   (let* ((height (overlay-get company-pseudo-tooltip-overlay 'company-height))
-         (row (overlay-get company-pseudo-tooltip-overlay 'company-row))
          (column (overlay-get company-pseudo-tooltip-overlay 'company-column))
          (oldwidth (overlay-get company-pseudo-tooltip-overlay 'company-width))
          (ovls (overlay-get company-pseudo-tooltip-overlay 'company-line-overlays))
@@ -2152,15 +2149,15 @@ current window."
          (adjcol (company--adjust-column column width))
          (inhibit-point-motion-hooks t))
     (save-excursion
-      (move-to-window-line row)
       (loop for ovl in ovls
             do (let ((line (pop lines)))
                  (overlay-put ovl 'company-line line)
-                 (if (and line (> (length line) 0)
+                 (when (and line (> (length line) 0)
                           (or (not (= width oldwidth))
                               (> nls 0)))
-                     (company--create-line-overlay adjcol line ovl)
-                   (vertical-motion 1)))))
+                   (goto-char (overlay-start ovl))
+                   (vertical-motion 0)
+                   (company--create-line-overlay adjcol line ovl)))))
     (overlay-put company-pseudo-tooltip-overlay 'company-width width)))
 
 (defun company-pseudo-tooltip-hide ()
@@ -2257,7 +2254,7 @@ current window."
 (defun company-preview-show-at-point (pos)
   (company-preview-hide)
 
-  (setq company-preview-overlay (make-overlay pos pos))
+  (setq company-preview-overlay (make-overlay pos (1+ pos)))
 
   (let ((completion (nth company-selection company-candidates)))
     (setq completion (propertize completion 'face 'company-preview))
@@ -2278,7 +2275,10 @@ current window."
          (not (equal completion ""))
          (add-text-properties 0 1 '(cursor t) completion))
 
-    (overlay-put company-preview-overlay 'after-string completion)
+    (if (eobp)
+        (overlay-put company-preview-overlay 'after-string completion)
+      (overlay-put company-preview-overlay 'display
+                   (concat completion (buffer-substring pos (1+ pos)))))
     (overlay-put company-preview-overlay 'window (selected-window))))
 
 (defun company-preview-hide ()

company-tests.el
@@ -312,9 +312,8 @@
             (company-candidates '("123")))
         (company-preview-show-at-point (point))
         (let ((ov company-preview-overlay))
-          (should (null (overlay-get ov 'display)))
-          (should (eq (overlay-start ov) (overlay-end ov)))
-          (should (string= (overlay-get ov 'after-string) "123")))))))
+          (should (string= (overlay-get ov 'display) "123\n"))
+          (should (null (overlay-get ov 'after-string))))))))
 
 (ert-deftest company-pseudo-tooltip-show-with-annotations ()
   :tags '(interactive)

commit de66b1306e102914c2e922efc34a032808f6b9f4

Fix performance regression caused by 10069f3.
Apparently using one big overlay to disable `display' property
from (point) to (point-max) will slow to a crawl on large
files. We need to be more conservative with the outer overlay
placement.

* company.el(company-pseudo-tooltip-show): Progressively enlarge
  outer overlay until we cover the correct number of lines or
  (eobp) has been reached.

company.el
@@ -2096,23 +2096,28 @@ current window."
            (inhibit-point-motion-hooks t)
            (beg (point))
            (firstrow (if above (- row height) row))
+           (lastrow (+ firstrow height))
            (modified (buffer-modified-p))
-           (ovl (make-overlay beg (point-max)))
-           lastrow pad padstart tick line-overlays)
+           (pad 0)
+           ovl padstart tick line-overlays)
 
+      (move-to-window-line firstrow)
+      (setq beg (point))
+      (move-to-window-line lastrow)
+      (setq ovl (make-overlay beg (point)))
       (overlay-put ovl 'priority 99)
       (overlay-put ovl 'display t)
-      (move-to-window-line (+ firstrow height))
-      (setq lastrow (company--row)
-            pad (- (+ firstrow height) lastrow)
-            padstart (point))
-      (if (> pad 0)
+      (while (and (< (company--row) lastrow) (not (eobp)))
+        (move-to-window-line lastrow)
+        (move-overlay ovl beg (point)))
+      (when (eobp)
+        (setq padstart (point)
+              pad (- lastrow (company--row)))
+        (when (> pad 0)
           (let ((buffer-undo-list t)
                 (inhibit-modification-hooks t))
-            (goto-char (point-max))
             (insert (make-string pad ?\n))
-            (setq company--point-max (point-max)))
-        (move-overlay ovl beg padstart))
+            (setq company--point-max (point-max)))))
       (setq tick (buffer-chars-modified-tick))
       (overlay-recenter (overlay-end ovl))
       (move-to-window-line firstrow)