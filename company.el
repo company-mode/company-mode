a4ac0dead8e9cb440c1f8aec9141d6c64bad4933
Dmitry Gutov <dgutov@yandex.ru>
2015-01-23 01:02:35 +0200

    Bump the version

--- company.el
+++ company.el
@@ -5,7 +5,7 @@
 ;; Author: Nikolaj Schumacher
 ;; Maintainer: Dmitry Gutov <dgutov@yandex.ru>
 ;; URL: http://company-mode.github.io/
-;; Version: 0.8.9-cvs
+;; Version: 0.8.9
 ;; Keywords: abbrev, convenience, matching
 ;; Package-Requires: ((emacs "24.1") (cl-lib "0.5"))
 
