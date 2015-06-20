a053848b9a64cd6b6c1fa5c8f442e8730275e2d2
Dmitry Gutov <dgutov@yandex.ru>
2015-06-20 06:55:35 +0300

    Sort alphabetically, to appease a test

--- company-keywords.el
+++ company-keywords.el
@@ -211,7 +211,7 @@
      "abstract" "case" "catch" "class" "def" "do" "else" "extends" "false"
      "final" "finally" "for" "forSome" "if" "implicit" "import" "lazy" "match"
      "new" "null" "object" "override" "package" "private" "protected"
-     "return" "sealed" "super" "this" "throw" "trait" "try" "true" "type" "val"
+     "return" "sealed" "super" "this" "throw" "trait" "true" "try" "type" "val"
      "var" "while" "with" "yield")
     ;; aliases
     (js2-mode . javascript-mode)
