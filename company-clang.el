b0a946db2c40c4cdba58d4a61b42468db7823ba5
Dmitry Gutov <dgutov@yandex.ru>
2015-06-21 21:53:57 +0300

    company-clang--start-process: Only erase buffer when there's no existing process
    Fixes #368

--- company-clang.el
+++ company-clang.el
@@ -203,11 +203,11 @@ or automatically through a custom `company-clang-prefix-guesser'."
         (buf (get-buffer-create "*clang-output*"))
         ;; Looks unnecessary in Emacs 25.1 and later.
         (process-adaptive-read-buffering nil))
-    (with-current-buffer buf
-      (erase-buffer)
-      (setq buffer-undo-list t))
     (if (get-buffer-process buf)
         (funcall callback nil)
+      (with-current-buffer buf
+        (erase-buffer)
+        (setq buffer-undo-list t))
       (let ((process (apply #'start-process "company-clang" buf
                             company-clang-executable args)))
         (set-process-sentinel
