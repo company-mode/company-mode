21da29bcb532775461bf1752d10b2e0d18ea5791
Dmitry Gutov <dgutov@yandex.ru>
2015-11-29 05:57:06 +0200

    Update copyright

--- company-yasnippet.el
+++ company-yasnippet.el
@@ -1,6 +1,6 @@
 ;;; company-yasnippet.el --- company-mode completion backend for Yasnippet
 
-;; Copyright (C) 2014  Free Software Foundation, Inc.
+;; Copyright (C) 2014, 2015  Free Software Foundation, Inc.
 
 ;; Author: Dmitry Gutov
 
