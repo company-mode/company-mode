c12216bc2f52f60a3ed90c07186d894e9a143de6
Dmitry Gutov <dgutov@yandex.ru>
2016-02-29 01:08:56 +0200

    re-sort

--- company-keywords.el
+++ company-keywords.el
@@ -36,7 +36,7 @@
   ;; Please contribute corrections or additions.
   `((c++-mode
      "alignas" "alignof" "asm" "auto" "bool" "break" "case" "catch" "char"
-     "char16_t" "char32_t" "class" "const" "constexpr" "const_cast" "continue"
+     "char16_t" "char32_t" "class" "const" "const_cast" "constexpr" "continue"
      "decltype" "default" "delete" "do" "double" "dynamic_cast" "else" "enum"
      "explicit" "export" "extern" "false" "final" "float" "for" "friend"
      "goto" "if" "inline" "int" "long" "mutable" "namespace" "new" "noexcept"
