971eea5ba3ff339ea6129cebff1fa2457fc9a6dc
Dmitry Gutov <dgutov@yandex.ru>
2016-06-20 05:22:07 +0300

    Bump copyright

--- test/core-tests.el
+++ test/core-tests.el
@@ -1,6 +1,6 @@
 ;;; core-tests.el --- company-mode tests  -*- lexical-binding: t -*-
 
-;; Copyright (C) 2015  Free Software Foundation, Inc.
+;; Copyright (C) 2015, 2016  Free Software Foundation, Inc.
 
 ;; Author: Dmitry Gutov
 
