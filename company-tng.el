company-mode/company-mode

commit 66a9e6540ce20bf75d397924d7760a385b5d9e2f

company-tng: Use 'after-string when the prefix is empty
The 'display property only works when an overlay's length is not zero. There can
be cases where `company-prefix' will be empty (for example, when the completion
was triggered manually). In that case, use the 'after-string property instead.

company-tng.el
@@ -83,6 +83,9 @@ confirm the selection and finish the completion."
            (selected (nth company-selection company-candidates))
            (prefix (length company-prefix)))
        (move-overlay ov (- (point) prefix) (point))
+       (overlay-put ov
+                    (if (= prefix 0) 'after-string 'display)
+                    (and company-selection-changed selected))
        (overlay-put ov 'display (and company-selection-changed selected))))
     (hide
      (when company-tng--overlay

commit eaddef478b748bbda87e1420b5e0cac50c16fad9

company-tng: Remove redundant call to overlay-put
Was left there by mistake, it's not needed

Fixes: 66a9e65 (company-tng: Use 'after-string when the prefix is empty)

company-tng.el
@@ -88,8 +88,7 @@ confirm the selection and finish the completion."
        (move-overlay ov (- (point) prefix) (point))
        (overlay-put ov
                     (if (= prefix 0) 'after-string 'display)
-                    (and company-selection-changed selected))
-       (overlay-put ov 'display (and company-selection-changed selected))))
+                    (and company-selection-changed selected))))
     (hide
      (when company-tng--overlay
        (delete-overlay company-tng--overlay)