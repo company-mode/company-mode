3262c183aa9eaf446e5562464659ef994ebc4268
stephen <sawakitaeiji233@gmail.com>
2020-03-16 12:02:42 +0800

    use symbol-value to get major-mode value

--- company-yasnippet.el
+++ company-yasnippet.el
@@ -99,17 +99,17 @@
 
 (defun company-yasnippet--doc (arg)
   (let ((template (get-text-property 0 'yas-template arg))
-        (mode (format "%s" major-mode)))
+        (mode (symbol-value 'major-mode)))
     (with-current-buffer (company-doc-buffer)
       (yas-minor-mode 1)
       (yas-expand-snippet (yas--template-content template))
       (delay-mode-hooks
         (let ((inhibit-message t))
-          (if (string-equal mode "web-mode")
+          (if (string-equal mode 'web-mode)
                 (progn
-                  (setq mode "html-mode")
-                  (funcall (intern mode)))
-            (funcall (intern mode))))
+                  (setq mode 'html-mode)
+                  (funcall mode))
+            (funcall mode)))
          (ignore-errors (font-lock-ensure)))
       (current-buffer))))
 
