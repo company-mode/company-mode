73c3c37dc185f0f2d6f5d977dc1d161af9cf8e7d
YE <yet@ego.team>
2021-09-26 12:45:42 +0300

    Instruct makefile-mode tabs indentation

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,4 +2,5 @@
          (fill-column . 80)
          (sentence-end-double-space . t)
          (emacs-lisp-docstring-fill-column . 75)
-         (project-vc-merge-submodules . nil))))
+         (project-vc-merge-submodules . nil)))
+ (makefile-mode . ((indent-tabs-mode . t))))
