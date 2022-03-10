adc519581409af7644eacb6e65cf0a062810d9a9
Dmitry Gutov <dgutov@yandex.ru>
2022-03-11 01:13:49 +0200

    Re-sort keywords; fix typo

--- company-keywords.el
+++ company-keywords.el
@@ -232,19 +232,19 @@
      "until" "use" "utime" "values" "vec" "wait" "waitpid"
      "wantarray" "warn" "while" "write" "x" "xor" "y")
     (php-mode ;; https://www.php.net/manual/reserved.php
-     "Closure" "Generator" "Exception" "Error" "Throwable"
+     "Closure" "Error" "Exception" "Generator" "Throwable"
      "__CLASS__" "__DIR__" "__FILE__" "__FUNCTION__" "__LINE__" "__METHOD__"
      "__NAMESPACE__" "__TRAIT__"
      "abstract" "and" "array" "as" "bool" "break" "callable" "case" "catch"
      "class" "clone" "const" "continue" "declare" "default" "die" "do" "echo"
      "else" "elseif" "empty" "enddeclare" "endfor" "endforeach" "endif"
-     "endswitch" "endwhile" "enum" "eval" "exit" "extends" "false" "float"
-     "final" "finally" "for" "foreach" "fn" "function" "global" "goto" "if"
+     "endswitch" "endwhile" "enum" "eval" "exit" "extends" "false" "final" "finally"
+     "float" "fn" "for" "foreach" "function" "global" "goto" "if"
      "implements" "include" "include_once" "instanceof" "insteadof" "interface"
      "isset" "iterable" "list" "match" "namespace" "new" "null" "object" "or"
      "print" "private" "protected" "public" "readonly" "require" "require_once"
      "return" "self" "static" "string" "switch" "this" "throw" "trait" "true"
-     "try" "unset" "use" "var" "void" "while" "xor" "yield" "yeild from")
+     "try" "unset" "use" "var" "void" "while" "xor" "yield" "yield from")
     (python-mode
      ;; https://docs.python.org/3/reference/lexical_analysis.html#keywords
      "False" "None" "True" "and" "as" "assert" "break" "class" "continue" "def"
