cfc759a7497fadc6d6b0f3814384a02c904e3728
Billy.Zheng <vil963@gmail.com>
2022-08-14 16:41:20 +0800

    Add Crystal-mode into company-keywords

--- company-keywords.el
+++ company-keywords.el
@@ -69,6 +69,15 @@
      "int" "long" "register" "restrict" "return" "short" "signed" "sizeof"
      "static" "struct" "switch" "typedef" "union" "unsigned" "void" "volatile"
      "while")
+    (crystal-mode
+     ;; from https://github.com/crystal-lang/crystal-book/issues/124#issuecomment-1008311227
+    "abstract" "alias" "annotation" "as" "as?" "asm" "begin" "break" "case" "class"
+    "def" "do" "else" "elsif" "end" "ensure" "enum" "extend" "false" "for" "fun"
+    "if" "in" "include" "instance_sizeof" "is_a?" "lib" "macro" "module" "next"
+    "nil" "nil?" "of" "offsetof" "out" "pointerof" "private" "protected" "require"
+    "rescue" "responds_to?" "return" "select" "self" "sizeof" "struct" "super"
+    "then" "true" "type" "typeof" "uninitialized" "union" "unless" "until" "verbatim"
+    "when" "while" "with" "yield")
     (csharp-mode
      "abstract" "add" "alias" "as" "base" "bool" "break" "byte" "case"
      "catch" "char" "checked" "class" "const" "continue" "decimal" "default"
