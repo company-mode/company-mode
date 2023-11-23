9b21604d19696de2c79ee28931620839b3a908b4
Dmitry Gutov <dmitry@gutov.dev>
2023-11-23 20:42:43 +0200

    Re-fix #202 with 'eval' again; now lexical-style
    Related: #1429

--- company-bbdb.el
+++ company-bbdb.el
@@ -23,9 +23,7 @@
 (require 'cl-lib)
 
 (declare-function bbdb-record-get-field "bbdb")
-(declare-function bbdb-records "bbdb")
 (declare-function bbdb-dwim-mail "bbdb-com")
-(declare-function bbdb-search "bbdb-com")
 
 (defgroup company-bbdb nil
   "Completion backend for BBDB."
@@ -40,7 +38,9 @@
   (cl-mapcan (lambda (record)
                (mapcar (lambda (mail) (bbdb-dwim-mail record mail))
                        (bbdb-record-get-field record 'mail)))
-             (bbdb-search (bbdb-records) :all-names arg :mail arg)))
+             (eval `(let ((arg ,arg))
+                      (bbdb-search (bbdb-records) :all-names arg :mail arg))
+                   t)))
 
 ;;;###autoload
 (defun company-bbdb (command &optional arg &rest _ignore)
