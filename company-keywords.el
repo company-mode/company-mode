982fcb1f7abe63d3c9c8f87efb56dda63dc9f034
Guyslain Naves <guyslain@gmail.com>
2023-12-07 11:52:07 +0100

    Update company-keywords.el with OCaml keywords

--- company-keywords.el
+++ company-keywords.el
@@ -403,7 +403,21 @@
      "i16" "i32" "i64" "include" "list" "map" "oneway" "optional" "required"
      "service" "set" "string" "struct" "throws" "typedef" "void"
      )
+    (tuareg-mode 
+     ;; ocaml, from https://v2.ocaml.org/manual/lex.html#sss:keywords
+     "and" "as" "assert" "asr" "begin" "class"
+     "constraint" "do" "done" "downto" "else" "end" 
+     "exception" "external" "false" "for" "fun" "function"
+     "functor" "if" "in" "include" "inherit" "initializer"
+     "land" "lazy" "let" "lor" "lsl" "lsr"
+     "lxor" "match" "method" "mod" "module" "mutable"
+     "new" "nonrec" "object" "of" "open" "or"
+     "private" "rec" "sig" "struct" "then" "to" 
+     "true" "try" "type" "val" "virtual" "when" 
+     "while" "with" 
+    )
     ;; aliases
+    (caml-mode . tuareg-mode)
     (js2-mode . javascript-mode)
     (js2-jsx-mode . javascript-mode)
     (espresso-mode . javascript-mode)
